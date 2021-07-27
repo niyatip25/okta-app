f065e73f62f73e0a94e57fc58c773f06c32445e6
niyatip25 <niyatip25@gmail.com>
2021-07-27 23:54:51 +0530

    Used ForEach for all three array of objects and concatinated in final array.

--- src/Flow/App1Elements.js
+++ src/Flow/App1Elements.js
@@ -2,29 +2,22 @@
 import { jsonAPI } from "./jsonAPI";
 
 var dataLabels = jsonAPI[0].propertyMappings;
-console.log("type of dataLabels: ", typeof dataLabels);
 console.log("dataLabels: ", dataLabels);
-
-// const getLabel = (index, fieldType) => {
-//   if (fieldType === "targetField") {
-//     return dataLabels[index].targetField;
-//   } else if (fieldType === "sourceExpression") {
-//     return dataLabels[index].sourceExpression;
-//   }
-// };
 
 const typeInput = "input";
 const typeOutput = "output";
 const sourcePosLeft = "left";
 const sourcePosRight = "right";
 
-var flowInputElementsArray = [];
-var flowOutputElementsArray = [];
-var mindmapArray = [];
+var LeftNodes = [];
+var RightNodes = [];
+var MindMaps = [];
 
-flowInputElementsArray = dataLabels.map(function (dataLabel, index) {
+dataLabels.forEach((data, index) => {
   var data1 = { label: dataLabels[index].sourceExpression };
-  var flowElement = {
+  var data2 = { label: dataLabels[index].targetField };
+
+  LeftNodes.push({
     id: index,
     data: data1,
     type: typeInput,
@@ -33,14 +26,10 @@ flowInputElementsArray = dataLabels.map(function (dataLabel, index) {
     style: {
       overflowWrap: "break-word",
     },
-  };
-  return flowElement;
-});
+  });
 
-flowOutputElementsArray = dataLabels.map(function (dataLabel, index) {
-  var data2 = { label: dataLabels[index].targetField };
-  var flowElement = {
-    id: index + dataLabels.length + 1,
+  RightNodes.push({
+    id: index + dataLabels.length,
     data: data2,
     type: typeOutput,
     targetPosition: sourcePosLeft,
@@ -48,68 +37,22 @@ flowOutputElementsArray = dataLabels.map(function (dataLabel, index) {
     style: {
       overflowWrap: "break-word",
     },
-  };
-  return flowElement;
-});
+  });
 
-mindmapArray = dataLabels.map(function (dataLabel, index) {
-  var flowElement = {
+  MindMaps.push({
     id: "e1-" + index,
     source: index,
-    target: dataLabels.length + (index + 1),
+    target: dataLabels.length + index,
     animated: true,
-  };
-  return flowElement;
+  });
 });
 
-var finalInputOutputArray = flowInputElementsArray.concat(
-  flowOutputElementsArray
-);
-var finalArray = finalInputOutputArray.concat(mindmapArray);
+console.log("LeftNodes: ", LeftNodes);
+console.log("RightNodes: ", RightNodes);
+console.log("MindMaps: ", MindMaps);
+
+var a = LeftNodes.concat(RightNodes);
+var finalArray = a.concat(MindMaps);
 console.log("finalArray: ", finalArray);
-console.log("flowOutputElementsArray: ", typeof flowOutputElementsArray);
-console.log("flowInputElementsArray: ", typeof flowInputElementsArray);
 
 export const flowElements = finalArray;
-
-// for (var i = 1; i <= dataLabels.length; i++) {
-//   var data1 = { label: dataLabels[i - 1].sourceExpression };
-//   flowElementsArray.push({
-//     id: i,
-//     data: data1,
-//     type: typeInput,
-//     sourcePosition: sourcePosRight,
-//     position: { x: 250, y: (i - 1) * 60 },
-//     style: {
-//       overflowWrap: "break-word",
-//     },
-//   });
-// }
-
-// finalArray = [
-//   ...flowInputElementsArray2.flowOutputElementsArray2,
-//   flowOutputElementsArray2,
-// ];
-
-// for (var j = dataLabels.length + 1; j <= dataLabels.length * 2; j++) {
-//   var data2 = { label: dataLabels[j - dataLabels.length - 1].targetField };
-//   flowElementsArray.push({
-//     id: j,
-//     data: data2,
-//     type: typeOutput,
-//     targetPosition: sourcePosLeft,
-//     position: { x: 750, y: (j - dataLabels.length - 1) * 60 },
-//     style: {
-//       overflowWrap: "break-word",
-//     },
-//   });
-// }
-
-// for (var k = 1; k <= dataLabels.length; k++) {
-//   flowElementsArray2.push({
-//     id: "e1-" + k,
-//     source: k,
-//     target: dataLabels.length + k,
-//     animated: true,
-//   });
-// }
